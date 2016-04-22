b748182abb93b0c188ed43ac430b98ffc7b44777
bho008 <bho008@ucr.edu>
2016-04-21 19:27:50 -0700

    Created some python functions to grab stationary and moving obstacles. Need help cleaning up exceptions :(

--- client/missionplanner/clientproxy.py
+++ client/missionplanner/clientproxy.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 from interop import AsyncClient
 from interop import Telemetry
+from interop import StationaryObstacle
+from interop import Client
 from time import time
 import argparse
 
@@ -12,7 +14,6 @@ except ImportError:
     from SimpleXMLRPCServer import SimpleXMLRPCServer
 
 __author__ = 'Joseph Moster'
-
 
 class RelayService:
     def __init__(self, url, username, password):
@@ -34,21 +35,37 @@ class RelayService:
 
         return True
 
+    def get_obstacles(self):
+        async_future = self.client.get_obstacles()
+        async_stationary, async_moving = async_future.result()
+        #print("here")
+        # stat_ob, moving_ob = self.client.get_obstacles()
+        async_radii_stationary = [o.cylinder_radius for o in async_stationary]
+        async_lat_stationary = [o.latitude for o in async_stationary]
+        async_lng_stationary = [o.longitude for o in async_stationary]
+        async_radii_moving = [o.sphere_radius for o in async_moving]
+        async_lat_moving = [o.latitude for o in async_moving]
+        async_lng_moving = [o.longitude for o in async_moving]
+        
+        
+        return async_radii_stationary, async_lat_stationary, async_lng_stationary, async_radii_moving, async_lat_moving, async_lng_moving
+	
+	def get_moving_obstacles(self):
+	
+		return True
+
     def server_info(self):
         info = self.client.get_server_info().result()
         return str(info.message)
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(
-        description='AUVSI SUAS Server Interface Relay')
-    parser.add_argument(
-        '--url',
+    parser = argparse.ArgumentParser(description='AUVSI SUAS Server Interface Relay')
+    parser.add_argument('--url',
         dest='url',
         help='Interoperability Server URL, example: http://10.10.130.10:80',
         required=True)
-    parser.add_argument(
-        '--username',
+    parser.add_argument('--username',
         dest='username',
         help='Interoperability Username, example: calpoly-broncos',
         required=True)
@@ -62,8 +79,7 @@ if __name__ == '__main__':
                          username=cmd_args.username,
                          password=cmd_args.password)
 
-    server = SimpleXMLRPCServer(
-        ('127.0.0.1', 9000),
+    server = SimpleXMLRPCServer(('127.0.0.1', 9000),
         logRequests=True,
         allow_none=True)
     server.register_instance(relay)
